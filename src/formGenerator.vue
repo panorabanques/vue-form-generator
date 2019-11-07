0e4a250b5e5c26d8a0e3bb57bf4376bc3afed518
Paul-Élie Testud <paul-elie.testud@panorabanques.com>
2019-11-07 14:49:46 +0100

    feat(group): add group visibility fn

--- src/formGenerator.vue
+++ src/formGenerator.vue
@@ -5,7 +5,7 @@ div.vue-form-generator(v-if='schema != null')
 			form-group(v-if='fieldVisible(field)', :vfg="vfg", :field="field", :errors="errors", :model="model", :options="options", @validated="onFieldValidated", @model-updated="onModelUpdated")
 
 	template(v-for='group in groups')
-		fieldset(:is='tag', :class='getFieldRowClasses(group)')
+		fieldset(:is='tag', v-if='groupVisible(group)', :class='getFieldRowClasses(group)')
 			legend(v-if='group.legend') {{ group.legend }}
 			template(v-for='field in group.fields')
 				form-group(v-if='fieldVisible(field)', :vfg="vfg", :field="field", :errors="errors", :model="model", :options="options", @validated="onFieldValidated", @model-updated="onModelUpdated")
@@ -204,6 +204,16 @@ export default {
 				child.clearValidationErrors();
 			});
 		},
+
+		groupVisible(group) {
+			if (isFunction(group.visible)) {
+				return group.visible.call(this, this.model, group, this);
+			}
+			if (isNil(group.visible)) {
+				return true;
+			}
+			return group.visible;
+		}
 	}
 };
 </script>
